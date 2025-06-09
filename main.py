9f2aced1e1218ef3ed88b77d4c9b30c7a2416ec2
heckardkyle <heckardkyle@yahoo.com>
2025-06-08 18:43:06 -0700

    Added using flags for arguments. Added --verbose to show usage metadata

--- main.py
+++ main.py
@@ -2,31 +2,52 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
-    if len(sys.argv) < 2 or len(sys.argv) > 2:
+    valid_flags = [
+        "--verbose"
+    ]
+
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
         print("Usage: python3 main.py <prompt>")
         print("The prompt needs to be contained in quotations")
         print('ex:   python3 main.py "Tell me a joke"')
         sys.exit(1)
 
+    used_flags = []
+
+    for i in range (2, len(sys.argv)): 
+        if sys.argv[i] not in valid_flags:
+            print(f"{sys.argv[i]} is not a valid argument")
+            print("Valid flags:")
+            for flag in valid_flags:
+                print(f"    {flag}")
+                sys.exit(1)
+        used_flags.append(sys.argv[i])
+
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
 
-    prompt = sys.argv[1]
+    user_prompt = sys.argv[1]
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]  
 
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents=prompt
+        contents=messages
     )
+
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+
+    if "--verbose" in used_flags:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+
     print(response.text)
-
-    prompt_token_count = response.usage_metadata.prompt_token_count
-    response_token_count = response.usage_metadata.candidates_token_count
-
-    print(f"Prompt tokens: {prompt_token_count}")
-    print(f"Response tokens: {response_token_count}")
 
 main()
